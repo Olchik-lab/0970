e72213f79716de50844efbfa53969ceb905df88a
Olchik-lab <77689889+Olchik-lab@users.noreply.github.com>
2021-08-27 14:16:24 +0300

    Create Yandex_Bot
    Создала яндекс бот

---
+++ yandexBot.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         Yandex_Bot
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       TsapovaOlga
+// @match        https://ya.ru/
+// @grant        none
+// ==/UserScript==
+
+let keywords = ["как звучит флейта","кларнет"];
+let btn = document.getElementsByClassName("button mini-suggest__button button_theme_search")[0];
+let links = document.links;
+let keyword = keywords[getRandom(0,keywords.length)];
+document.getElementsByName("text")[0].value = keyword;
+
+
+if (btn !==undefined) {
+    btn.click();
+}else {
+    for (let i=0; i<links.length; i++){
+        if (links[i].href.includes('музыкалка-онлайн.рф')){
+            let link = links[i];
+            link.click();
+            console.log("Нашел строку"+links[i]);
+            break;
+        }
+    }
+}
+function getRandom(min,max){
+return Math.floor(Math.random()*(max-min)+min)
+}
